e29d52461f360467a426223b6d81ccc80bdce886
vitaliizuzka <vitaliizuzka@gmail.com>
2025-07-22 17:59:09 +0300

    add config server

--- src/main/java/ru/aston/user_service_spring_boot/UserServiceSpringBootApplication.java
+++ src/main/java/ru/aston/user_service_spring_boot/UserServiceSpringBootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserServiceSpringBootApplication {
-
     public static void main(String[] args) {
         var context =  SpringApplication.run(UserServiceSpringBootApplication.class, args);
     }
